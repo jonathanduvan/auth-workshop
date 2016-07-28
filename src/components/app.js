19f618dd876dab1505596896ddb4113957d7266f
jonathanduvan <jonathan.d.gonzalez.18@dartmouth.edu>
2016-07-28 15:14:21 -0400

    have both logins setup

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 // example class based component (smart component)
 class App extends Component {
@@ -6,15 +8,74 @@ class App extends Component {
     super(props);
 
     this.state = {
-
+      google: false,
+      googleName: '',
+      googleEmail: '',
+      googleImage: '',
     };
+
+    this.onGoogleLogin = this.onGoogleLogin.bind(this);
   }
 
+  // What happens after someone logs in
+  onGoogleLogin(response) {
+    if (response) {
+      const user = response.getBasicProfile();
+      this.setState({
+        google: true,
+        googleName: user.getName(),
+        googleEmail: user.getEmail(),
+        googleImage: user.getImageUrl(),
+      });
+    }
+  }
+  onFacebookLogin(response) {
+    if (response.status !== 'not_authorized' && response.status !== 'unknown') {
+      document.getElementById('facebook').innerHTML = `Welcome, ${response.name}! `;
+      document.getElementById('facebook').innerHTML += `<img src="${response.picture.data.url}"/>`;
+      document.getElementById('facebook').style = 'display: block';
+    } else {
+      document.getElementById('facebook').style = 'display: block';
+    }
+  }
 
+  renderGoogle() {
+    if (this.state.google) {
+      return (
+        <div>
+          <ul>
+            <li>Thanks for logging in! </li>
+            <li>Your Name: {this.state.googleName}</li>
+            <li>Your Email: {this.state.googleEmail}</li>
+            <li>Your Photo: <img src={this.state.googleImage} alt="N/A" /></li>
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <GoogleLogin
+          clientId="842390445947-ih8v6tbac7tmhlgdj3sdf5it62a5dnl9.apps.googleusercontent.com"
+          buttonText="LOGIN WITH GOOGLE"
+          callback={this.onGoogleLogin}
+        />
+      );
+    }
+  }
   render() {
     // Add in FacebookLogin, GoogleLogin and FirebaseApp
     return (
       <div id="login">
+        <div id="facebook">
+          <FacebookLogin
+            appId="1719436634940250"
+            autoLoad
+            fields="name,email,picture.type(large)"
+            callback={this.onFacebookLogin}
+          />
+        </div>
+        <div id="google">
+          {this.renderGoogle()}
+        </div>
       </div>
     );
   }
